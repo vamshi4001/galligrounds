48f3993da84d41499c04e50f01c9f450dc8b1fce
Vamshi <vamshi4001@gmail.com>
2012-12-21 22:43:56 +0530

    pwds changed

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '100Likes!');
-    define('DB_DATABASE', 'bragem');
+    define('DB_USER', 'USERNAME');
+    define('DB_PASSWORD', 'PASSWORD');
+    define('DB_DATABASE', 'DATABASENAME');
 ?>
